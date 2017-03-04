eabeb36a7e6b16202c89ac364edad48693d9d1c2
David de Tena <daviddetenacalvo@gmail.com>
2017-03-04 13:49:19 +0100

    Initial Commit

---
+++ BatLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BatLogin
+//
+//  Created by David de Tena on 04/03/2017.
+//  Copyright © 2017 David de Tena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
